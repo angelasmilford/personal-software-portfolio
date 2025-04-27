24536a49c8525e43ef4861bfb942aa2e46d55065
angelasmilford <angela.milford123@gmail.com>
2025-04-26 18:32:31 -1100

    My "Hello World" file for this week's Prove assignment.

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+print("Hello, World!") #outputs the text "Hello World"
+
+"""
+print == built-in Python function used to send output to the console or terminal
+"Hello World!" == string value that will be printed
+"""
