984e43ea142903cf45089f8dbaffa3b379436b48
angelasmilford <angela.milford123@gmail.com>
2025-04-29 08:09:34 -1100

    Made a few changes

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,12 @@
-#include <iostream> //includes the input/output stream library, which gives access to objects
-using namespace std; //tells the compiler to use standard namespace
+#include <iostream>  //includes the input/output stream library, which gives access to objects
+using namespace std; // tells the compiler to use standard namespace
 
-int main() { //defines the main function/entry point for the C++ program
-  cout << "Hello World!"; //prints "Hello World!" to the console
-  return 0; //ends the main function, and returns a 0 to the operating system
-} 
-/* 
+int main()
+{                         // defines the main function/entry point for the C++ program
+  cout << "Hello World!"; // prints "Hello World!" to the console
+  return 0;               // ends the main function, and returns a 0 to the operating system
+}
+/*
 int == function returns an integer, indicating that program ran successfully
 {} == opens and closes the main function
 cout == console output
