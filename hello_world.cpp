40dd2763e20656dbc110b856f994a89d005c8c9a
angelasmilford <angela.milford123@gmail.com>
2025-04-29 08:08:34 -1100

    my Hello World C++ file that simply prints "Hello World!" It contains comments, both multi-line and single line, for notes.

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream> //includes the input/output stream library, which gives access to objects
+using namespace std; //tells the compiler to use standard namespace
+
+int main() { //defines the main function/entry point for the C++ program
+  cout << "Hello World!"; //prints "Hello World!" to the console
+  return 0; //ends the main function, and returns a 0 to the operating system
+} 
+/* 
+int == function returns an integer, indicating that program ran successfully
+{} == opens and closes the main function
+cout == console output
+<< == insertion operator, which sends data to the output stream
+"Hello World!" == text string literal
+*/
